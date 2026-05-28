21789ec3882b043868021cc3e768010904de5d5b
Michelle Cheng <cheng.michelley@gmail.com>
2026-05-28 00:07:28 -0400

    Updated Code

--- exercises.py
+++ exercises.py
@@ -8,10 +8,12 @@
 # - The list should contain any three elements of your choice.
 # - Use a loop to print each element.
 
+
 def example_list_function():
-  example_list = ['element1', 'element2', 'element3']
-  for element in example_list:
-      print(element)
+    example_list = ["element1", "element2", "element3"]
+    for element in example_list:
+        print(element)
+
 
 # Call the function and print each element
 example_list_function()
@@ -22,16 +24,18 @@ example_list_function()
 # Assign the second student’s name to a variable named first_student.
 # Assign the last student’s name to a variable named last_student.
 
+
 def manage_students():
-  students = ['Nick', 'Matt', 'Sam']
+    students = ["Nick", "Matt", "Sam"]
 
-  first_student = students[1]
-  last_student = students[-1]
+    first_student = students[1]
+    last_student = students[-1]
 
-  return students, first_student, last_student
+    return students, first_student, last_student
+
 
 # Call the function and print the result
-print('Exercise 1:', manage_students())
+print("Exercise 1:", manage_students())
 
 # Exercise 2: Loop and String Concatenation
 #
@@ -39,99 +43,100 @@ print('Exercise 1:', manage_students())
 # Create a variable named meal and assign an empty string to it.
 # Use a for loop to iterate over the strings in foods and append each string to meal.
 
-def combine_foods():
-  foods = ('sushi', 'mango', 'avocado')
-  meal = ''
 
-  for food in foods:
-    meal += food + ' '
-  
-  return foods, meal
+def combine_foods():
+    foods = ("sushi", "mango", "avocado")
+    meal = ""
+
+    for food in foods:
+        meal += "".join([food, " "])
+
+    return foods, meal.strip()
+
 
 # Call the function and print the result
-print('Exercise 2:', combine_foods())
+print("Exercise 2:", combine_foods())
 
 # Exercise 3: Slicing Tuples
 #
 # Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
 
-def slice_foods():
-  foods = ('sushi', 'mango', 'avocado')
 
-  last_two_foods = foods[-2:]
+def slice_foods(foods):
 
-  return last_two_foods
+    last_two_foods = foods[-2:]
+
+    return last_two_foods
+
 
 # Call the function and print the result
-print('Exercise 3:', slice_foods())
+print("Exercise 3:", slice_foods(combine_foods()[0]))
 
 # Exercise 4: Dictionaries and String Formatting
 #
 # Create a dictionary named home_town containing the keys of city, state, and population.
 # Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
 
+
 def hometown_info():
-  home_town = {
-    'city' : 'Boston', 
-    'state' : 'Massachusetts', 
-    'population' : 13791391
-    }
+    home_town = {"city": "Boston", "state": "Massachusetts", "population": 13791391}
 
-  home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
+    home_town_message = f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}"
 
-  return home_town_message
+    return home_town, home_town_message
+
 
 # Call the function and print the result
-print('Exercise 4:', hometown_info())
+print("Exercise 4:", hometown_info())
 
 # Exercise 5: Iterating Over Dictionary Items
 #
 # Define an empty list named home_town_items.
 # Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
 
-def list_home_town_items():
-  home_town = {
-    'city' : 'Boston', 
-    'state' : 'Massachusetts', 
-    'population' : 13791391
-  }
 
-  home_town_items = []
+def list_home_town_items(home_town):
+    home_town_items = []
 
-  for key, value in home_town.items():
-    home_town_items.append(f"{key} = {value}")
+    for key, value in home_town.items():
+        home_town_items.append(f"{key} = {value}")
 
-  return home_town_items
+    return home_town_items
+
 
 # Call the function and print the result
-print('Exercise 5:', list_home_town_items())
+print("Exercise 5:", list_home_town_items(hometown_info()[0]))
 
 # Exercise 6: Celebrate Students
 #
 # Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
 # For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
 
+
 def create_awesome_students():
-  students = ['Nick', 'Matt', 'Sam']
+    students = ["Nick", "Matt", "Sam"]
 
-  awesome_students = [f"{student} is awesome!" for student in students]
+    awesome_students = [f"{student} is awesome!" for student in students]
 
-  return awesome_students
+    return awesome_students
+
 
 # Call the function and print the result
-print('Exercise 6:', create_awesome_students())
+print("Exercise 6:", create_awesome_students())
 
 # Exercise 7: Filter Foods
 #
 # Assign to a variable named foods_with_an_a the result of list comprehension that filters the foods tuple to only include food strings that contain the letter 'a'.
 # For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
 
+
 def filter_foods_with_a():
-  foods = ('sushi', 'mango', 'avocado')
+    foods = ("sushi", "mango", "avocado")
 
-  foods_with_an_a = [food for food in foods if 'a' in food]
+    foods_with_an_a = [food for food in foods if "a" in food]
 
-  return foods_with_an_a
+    return foods_with_an_a
+
 
 # Call the function and print the result
-print('Exercise 7:', filter_foods_with_a())
+print("Exercise 7:", filter_foods_with_a())
